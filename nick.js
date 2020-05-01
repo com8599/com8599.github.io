88f5bf8ca0f2d14920d8046b28742dc5f8b9cc7c
com8599 <com8599@gmail.com>
2020-05-01 15:28:46 +0900

    Update nick.js

--- nick.js
+++ nick.js
@@ -1,9 +1,13 @@
+
+  var name = '';  
   function nyehuing(length) {
-    var name = '';
     for(var i = 0; i < length; i++) {
       name += String.fromCharCode(Math.floor(Math.random()*11171)+44032);
     }
     return name;
   }
   
+  function relocation(){
+    return 'https://maple.gg/u/'+name;
+  }
 //https://maplestory.nexon.com/Ranking/World/Total?page=6875021

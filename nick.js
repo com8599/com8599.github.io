58ce110363b2efd7a3ec7a884cbe4309fc2b762c
com8599 <com8599@gmail.com>
2020-05-01 15:39:11 +0900

    Update nick.js

--- nick.js
+++ nick.js
@@ -4,10 +4,17 @@
     for(var i = 0; i < length; i++) {
       name += String.fromCharCode(Math.floor(Math.random()*11171)+44032);
     }
+    var getSpan = document.getElementsByClassName('spanName');
+    for(var i:getSpan){
+      i.innerHTML=name;
+    }
     return name;
   }
   
   function relocation(){
-    return 'https://maple.gg/u/'+name;
+    if(name == ''){
+      alert('닉네임이 정해지지 않았습니다.');
+    }else
+      return 'https://maple.gg/u/'+name;
   }
 //https://maplestory.nexon.com/Ranking/World/Total?page=6875021
